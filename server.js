e67cd6458692dc5eccc7f1e67c449a11fca26222
Ilfir Yakupov <philsmirnoff@gmail.com>
2022-05-26 16:59:29 -0400

    created sequilize models

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
+const db = require("./app/models");
+
+db.sequelize.sync({ force: true }).then(() => {
+  console.log("Drop and re-sync db.");
+});
+
 var corsOptions = {
   origin: "http://localhost:8081"
 };
@@ -11,6 +17,7 @@ app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 // simple route
+
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to bezkoder application." });
 });
